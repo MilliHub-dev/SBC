7a6dec25582829205fec8158e3591acc0b5e181a
Dr milli <100302736+drmilli@users.noreply.github.com>
2025-07-21 19:55:53 +0100

    Update index.jsx

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/react";
+//import { Container } from "@chakra-ui/react";
 import React from "react";
 import DashboardNav from "./components/DashboardNav/DashboardNav";
 
